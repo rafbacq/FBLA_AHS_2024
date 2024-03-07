8f69ce3797c182db77caa9c8a994c5841fd83683
notzapped <106430934+notzapped@users.noreply.github.com>
2024-03-06 22:59:56 -0500

    Update EmailServlet.java
    Added the functionality for sending emails

--- EmailServlet.java
+++ EmailServlet.java
@@ -30,15 +30,59 @@ public class EmailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     String verifySubject = "Atholton Solutions: Verify Your Account!";
     String passwordSubject = "Atholton Solutions: Reset Your Password.";
-    String verifyPage = "Hello. This is an email test.";
-    String passwordPage = "Hello. This is a password test.";
+    String verifyPage = "<!DOCTYPE html>\r\n"
+    		+ "<html>\r\n"
+    		+ "<head>\r\n"
+    		+ "<meta charset=\"ISO-8859-1\">\r\n"
+    		+ "<title>Atholton Solutions</title>\r\n"
+    		+ "<link rel=\"stylesheet\"\r\n"
+    		+ "	href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\"\r\n"
+    		+ "	integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\"\r\n"
+    		+ "	crossorigin=\"anonymous\">\r\n"
+    		+ "</head>\r\n"
+    		+ "<body style=\"background-color:#f3f3f3ff;\">\r\n"
+    		+ "	<header>\r\n"
+    		+ "	<nav class=\"navbar navbar-expand-md navbar-dark\">\r\n"
+    		+ "			<div>\r\n"
+    		+ "				<img class=\"school-logo\"\r\n"
+    		+ "					src=\"https://ahs.hcpss.org/sites/default/files/atholtonhs.png\"\r\n"
+    		+ "					alt=\"Logo image\" style=\"width: 80px\"> <a class=\"navbar-brand\"\r\n"
+    		+ "					style=\"color: black; vertical-align:middle; font-size:200%;\" href=\"https://ahs.hcpss.org/\"> Atholton Solutions </a>\r\n"
+    		+ "			</div>\r\n"
+    		+ "		</nav>\r\n"
+    		+ "		<br>\r\n"
+    		+ "		<div style=\"text-align:center\">\r\n"
+    		+ "			<image class=\"rounded-circle border border-success\"  src = \"https://static.hudl.com/users/temp/7082861_e91756197bb64e60b4b3850c4a1b85c1.jpg\" alt=\"AHS Logo\" style=\"height:100px; width:100px;\">\r\n"
+    		+ "		</div>\r\n"
+    		+ "	</header>\r\n"
+    		+ "	<div style=\"text-align:center;\">\r\n"
+    		+ "		<br>\r\n"
+    		+ "		<h1 style=\"text-align:center;\">Confirm Your Account</h1>\r\n"
+    		+ "		<br>\r\n"
+    		+ "		<div class=\"container col-md-5\" style=\"width:75%;\">\r\n"
+    		+ "			<div class=\"card\">\r\n"
+    		+ "				<div class=\"card-body rounded\" style=\"background-color:#fafafaff; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);\">\r\n"
+    		+ "					<b class=\"h5\">This email is being sent to confirm your creation of an account with Atholton Solutions</b>\r\n"
+    		+ "					<br>\r\n"
+    		+ "					<p>Here is your randomly generated six-digit code. Don't share it with anyone! </p>\r\n"
+    		+ "					<span class=\"container-fluid bg-success text-white\">?</span>\r\n"
+    		+ "					<p>If you dont recognize the name attached to this email, or if this was a mistake, please ignore this email </p>\r\n"
+    		+ "				</div>\r\n"
+    		+ "			</div>\r\n"
+    		+ "			<p>Atholton Solutions</p>\r\n"
+    		+ "		</div>\r\n"
+    		+ "	</div>\r\n"
+    		+ "	\r\n"
+    		+ "</body>\r\n"
+    		+ "</html>";
+    String passwordPage = "";
     
     /**
      * @see HttpServlet#HttpServlet()
      */
     public EmailServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        //TODO Auto-generated constructor stub
     }
 
 	/**
@@ -83,7 +127,13 @@ public class EmailServlet extends HttpServlet {
 		boolean passed = false;
 			String code = generateCode();
 			System.out.println(code);
-			passed = sendEmail(email, code, verifySubject, verifyPage);
+			
+			StringBuilder sb = new StringBuilder(verifyPage);
+			int ind = sb.indexOf("?");
+			sb.deleteCharAt(ind);
+			sb.insert(ind, code);
+			
+			passed = sendEmail(email, code, verifySubject, sb.toString());
 			
 			if(passed) {
 				
@@ -162,7 +212,7 @@ public class EmailServlet extends HttpServlet {
 			msg.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
 			
 			msg.setSubject(body);
-			msg.setText(message + " " + code);
+			msg.setContent(message, "text/html");
 			
 			Transport.send(msg);
 			System.out.println("email sent");
